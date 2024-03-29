95de9eaff272cba7ce4e956ba93f4943c5e17b70
Moy247 <103884305+Moy247@users.noreply.github.com>
2024-03-29 20:51:46 +0300

    Add files via upload

---
+++ bittisonunda.py
@@ -0,0 +1,481 @@
+from itertools import cycle
+from random import randrange
+from tkinter import Canvas, Tk, messagebox, font
+secim = input("İşlem yapmak istediğiniz numarayı girin (1/2/3/4/5): ")
+if secim == '1':
+    ...
+canvas_width = 800
+canvas_height = 400
+
+root = Tk()
+root.title("Egg Catcher")
+c = Canvas(root, width=canvas_width, height=canvas_height, background="deep sky blue")
+c.create_rectangle(-5, canvas_height-100, canvas_width+5, canvas_height+5, fill="sea green", width=0)
+c.create_oval(-80, -80, 120, 120, fill='orange', width=0)
+c.pack()
+
+color_cycle = cycle(["light blue", "light green", "light pink", "light yellow", "light cyan"])
+egg_width = 45
+egg_height = 55
+egg_score = 10
+egg_speed = 500
+egg_interval = 4000
+difficulty = 0.95
+catcher_color = "blue"
+catcher_width = 100
+catcher_height = 100
+catcher_startx = canvas_width / 2 - catcher_width / 2
+catcher_starty = canvas_height - catcher_height - 20
+catcher_startx2 = catcher_startx + catcher_width
+catcher_starty2 = catcher_starty + catcher_height
+
+catcher = c.create_arc(catcher_startx, catcher_starty, catcher_startx2, catcher_starty2, start=200, extent=140, style="arc", outline=catcher_color, width=3)
+game_font = font.nametofont("TkFixedFont")
+game_font.config(size=18)
+
+
+score = 0
+score_text = c.create_text(10, 10, anchor="nw", font=game_font, fill="darkblue", text="Score: "+ str(score))
+
+lives_remaining = 3
+lives_text = c.create_text(canvas_width-10, 10, anchor="ne", font=game_font, fill="darkblue", text="Lives: "+ str(lives_remaining))
+
+eggs = []
+
+def create_egg():
+    x = randrange(10, 740)
+    y = 40
+    new_egg = c.create_oval(x, y, x+egg_width, y+egg_height, fill=next(color_cycle), width=0)
+    eggs.append(new_egg)
+    root.after(egg_interval, create_egg)
+
+def move_eggs():
+    for egg in eggs:
+        (eggx, eggy, eggx2, eggy2) = c.coords(egg)
+        c.move(egg, 0, 10)
+        if eggy2 > canvas_height:
+            egg_dropped(egg)
+    root.after(egg_speed, move_eggs)
+
+def egg_dropped(egg):
+    eggs.remove(egg)
+    c.delete(egg)
+    lose_a_life()
+    if lives_remaining == 0:
+        messagebox.showinfo("Game Over!", "Final Score: "+ str(score))
+        root.destroy()
+
+def lose_a_life():
+    global lives_remaining
+    lives_remaining -= 1
+    c.itemconfigure(lives_text, text="Lives: "+ str(lives_remaining))
+
+def check_catch():
+    (catcherx, catchery, catcherx2, catchery2) = c.coords(catcher)
+    for egg in eggs:
+        (eggx, eggy, eggx2, eggy2) = c.coords(egg)
+        if catcherx < eggx and eggx2 < catcherx2 and catchery2 - eggy2 < 40:
+            eggs.remove(egg)
+            c.delete(egg)
+            increase_score(egg_score)
+    root.after(100, check_catch)
+
+def increase_score(points):
+    global score, egg_speed, egg_interval
+    score += points
+    egg_speed = int(egg_speed * difficulty)
+    egg_interval = int(egg_interval * difficulty)
+    c.itemconfigure(score_text, text="Score: "+ str(score))
+
+def move_left(event):
+    (x1, y1, x2, y2) = c.coords(catcher)
+    if x1 > 0:
+        c.move(catcher, -20, 0)
+
+def move_right(event):
+    (x1, y1, x2, y2) = c.coords(catcher)
+    if x2 < canvas_width:
+        c.move(catcher, 20, 0)
+
+c.bind("<Left>", move_left)
+c.bind("<Right>", move_right)
+c.focus_set()
+root.after(1000, create_egg)
+root.after(1000, move_eggs)
+root.after(1000, check_catch)
+root.mainloop()
+
+
+if secim == '2':
+    ...
+import turtle as t
+import random as rd
+
+t.bgcolor('yellow')
+
+caterpillar = t.Turtle()
+caterpillar.shape('square')
+caterpillar.color('red')
+caterpillar.speed(0)
+caterpillar.penup()
+caterpillar.hideturtle()
+
+leaf = t.Turtle()
+leaf_shape = ((0,0),(14,2),(18,6),(20,20),(6,18),(2,14))
+t.register_shape('leaf',leaf_shape)
+leaf.shape('leaf')
+leaf.color('green')
+leaf.penup()
+leaf.hideturtle()
+leaf.speed()
+
+game_started = False
+text_turtle = t.Turtle()
+text_turtle.write('Press SPACE to start',align='center',font=('Arial',16,'bold'))
+text_turtle.hideturtle()
+
+score_turtle = t.Turtle()
+score_turtle.hideturtle()
+score_turtle.speed(0)
+
+def outside_window():
+    left_wall = -t.window_width()/2
+    right_wall = t.window_width()/2
+    top_wall = t.window_height()/2
+    bottom_wall = -t.window_height()/2
+    (x,y) = caterpillar.pos()
+    outside = x < left_wall or  x > right_wall or  y < bottom_wall or y > top_wall
+    return outside
+
+def game_over():
+    caterpillar.color('yellow')
+    leaf.color('yellow')
+    t.penup()
+    t.hideturtle()
+    t.write('GAME OVER!',align='center' , font=('Aerial',30,'normal'))
+
+def display_score(current_score):
+    score_turtle.clear()
+    score_turtle.penup()
+    x = (t.window_width() / 2)-50
+    y = (t.window_height() / 2)-50
+    score_turtle.setpos(x,y)
+    score_turtle.write(str(current_score) , align = 'right',font=('Arial',40,'bold'))
+
+def place_leaf():
+    leaf.hideturtle()
+    leaf.setx(rd.randint(-200,200))
+    leaf.sety(rd.randint(-200,200))
+    leaf.showturtle()
+
+def start_game():
+    global game_started
+    if game_started:
+        return
+    game_started = True
+
+    score = 0
+    text_turtle.clear()
+
+    caterpillar_speed = 2
+    caterpillar_length = 3
+    caterpillar.shapesize(1,caterpillar_length,1)
+    caterpillar.showturtle()
+    display_score(score)
+    place_leaf()
+
+    while True:
+        caterpillar.forward(caterpillar_speed)
+        if caterpillar.distance(leaf)<20:
+            place_leaf()
+            caterpillar_length = caterpillar_length + 1
+            caterpillar.shapesize(1,caterpillar_length,1)
+            caterpillar_speed = caterpillar_speed + 1
+            score = score + 10
+            display_score(score)
+        if outside_window():
+            game_over()
+            break
+
+def move_up():
+    if caterpillar.heading() == 0 or caterpillar.heading() == 180:
+        caterpillar.setheading(90)
+
+def move_down():
+    if caterpillar.heading() == 0 or caterpillar.heading() == 180:
+        caterpillar.setheading(270)
+
+def move_left():
+    if caterpillar.heading() == 90 or caterpillar.heading() == 270:
+        caterpillar.setheading(180)
+
+def move_right():
+    if caterpillar.heading() == 90 or caterpillar.heading() == 270:
+        caterpillar.setheading(0)
+
+t.onkey(start_game,'space')
+t.onkey(move_up,'Up')
+t.onkey(move_right,'Right')
+t.onkey(move_down,'Down')
+t.onkey(move_left,'Left')
+t.listen()
+t.mainloop()
+
+if secim == '3':
+   ...
+from random import *
+from turtle import *
+
+from freegames import path
+
+car = path('car.gif')
+tiles = list(range(32)) * 2
+state = {'mark': None}
+hide = [True] * 64
+
+
+def square(x, y):
+    """Draw white square with black outline at (x, y)."""
+    up()
+    goto(x, y)
+    down()
+    color('black', 'white')
+    begin_fill()
+    for count in range(4):
+        forward(50)
+        left(90)
+    end_fill()
+
+
+def index(x, y):
+    """Convert (x, y) coordinates to tiles index."""
+    return int((x + 200) // 50 + ((y + 200) // 50) * 8)
+
+
+def xy(count):
+    """Convert tiles count to (x, y) coordinates."""
+    return (count % 8) * 50 - 200, (count // 8) * 50 - 200
+
+
+def tap(x, y):
+    """Update mark and hidden tiles based on tap."""
+    spot = index(x, y)
+    mark = state['mark']
+
+    if mark is None or mark == spot or tiles[mark] != tiles[spot]:
+        state['mark'] = spot
+    else:
+        hide[spot] = False
+        hide[mark] = False
+        state['mark'] = None
+
+
+def draw():
+    """Draw image and tiles."""
+    clear()
+    goto(0, 0)
+    shape(car)
+    stamp()
+
+    for count in range(64):
+        if hide[count]:
+            x, y = xy(count)
+            square(x, y)
+
+    mark = state['mark']
+
+    if mark is not None and hide[mark]:
+        x, y = xy(mark)
+        up()
+        goto(x + 2, y)
+        color('black')
+        write(tiles[mark], font=('Arial', 30, 'normal'))
+
+    update()
+    ontimer(draw, 100)
+
+
+shuffle(tiles)
+setup(420, 420, 370, 0)
+addshape(car)
+hideturtle()
+tracer(False)
+onscreenclick(tap)
+draw()
+done()
+
+if secim == '4':
+    ...
+from random import choice, random
+from turtle import *
+
+from freegames import vector
+
+
+def value():
+    """Randomly generate value between (-5, -3) or (3, 5)."""
+    return (3 + random() * 2) * choice([1, -1])
+
+
+ball = vector(0, 0)
+aim = vector(value(), value())
+state = {1: 0, 2: 0}
+
+
+def move(player, change):
+    """Move player position by change."""
+    state[player] += change
+
+
+def rectangle(x, y, width, height):
+    """Draw rectangle at (x, y) with given width and height."""
+    up()
+    goto(x, y)
+    down()
+    begin_fill()
+    for count in range(2):
+        forward(width)
+        left(90)
+        forward(height)
+        left(90)
+    end_fill()
+
+
+def draw():
+    """Draw game and move pong ball."""
+    clear()
+    rectangle(-200, state[1], 10, 50)
+    rectangle(190, state[2], 10, 50)
+
+    ball.move(aim)
+    x = ball.x
+    y = ball.y
+
+    up()
+    goto(x, y)
+    dot(10)
+    update()
+
+    if y < -200 or y > 200:
+        aim.y = -aim.y
+
+    if x < -185:
+        low = state[1]
+        high = state[1] + 50
+
+        if low <= y <= high:
+            aim.x = -aim.x
+        else:
+            return
+
+    if x > 185:
+        low = state[2]
+        high = state[2] + 50
+
+        if low <= y <= high:
+            aim.x = -aim.x
+        else:
+            return
+
+    ontimer(draw, 50)
+
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+listen()
+onkey(lambda: move(1, 20), 'w')
+onkey(lambda: move(1, -20), 's')
+onkey(lambda: move(2, 20), 'i')
+onkey(lambda: move(2, -20), 'k')
+draw()
+done()
+
+if secim == '5':
+    ...
+from random import randrange, seed
+from turtle import *
+
+from freegames import floor, square
+
+seed(0)
+bombs = {}
+shown = {}
+counts = {}
+
+
+def initialize():
+    """Initialize `bombs`, `counts`, and `shown` grids."""
+    for x in range(-250, 250, 50):
+        for y in range(-250, 250, 50):
+            bombs[x, y] = False
+            shown[x, y] = False
+            counts[x, y] = -1
+
+    for count in range(8):
+        x = randrange(-200, 200, 50)
+        y = randrange(-200, 200, 50)
+        bombs[x, y] = True
+
+    for x in range(-200, 200, 50):
+        for y in range(-200, 200, 50):
+            total = 0
+            for i in (-50, 0, 50):
+                for j in (-50, 0, 50):
+                    total += bombs[x + i, y + j]
+            counts[x, y] = total
+
+
+def stamp(x, y, text):
+    """Display `text` at coordinates `x` and `y`."""
+    square(x, y, 50, 'white')
+    color('black')
+    write(text, font=('Arial', 50, 'normal'))
+
+
+def draw():
+    """Draw the initial board grid."""
+    for x in range(-200, 200, 50):
+        for y in range(-200, 200, 50):
+            stamp(x, y, '?')
+
+
+def end():
+    """Draw the bombs as X's on the grid."""
+    for x in range(-200, 200, 50):
+        for y in range(-200, 200, 50):
+            if bombs[x, y]:
+                stamp(x, y, 'X')
+
+
+def tap(x, y):
+    """Respond to screen click at `x` and `y` coordinates."""
+    x = floor(x, 50)
+    y = floor(y, 50)
+
+    if bombs[x, y]:
+        end()
+        return
+
+    pairs = [(x, y)]
+
+    while pairs:
+        x, y = pairs.pop()
+        stamp(x, y, counts[x, y])
+        shown[x, y] = True
+
+        if counts[x, y] == 0:
+            for i in (-50, 0, 50):
+                for j in (-50, 0, 50):
+                    pair = x + i, y + j
+                    if not shown[pair]:
+                        pairs.append(pair)
+
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+initialize()
+draw()
+onscreenclick(tap)
+done()
